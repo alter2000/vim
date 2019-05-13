e892912110c81b917a3c78e83fe923bb68517d6c
Jor Bashllari <altnate2000@gmail.com>
2019-05-13 16:07:02 +0200

    misc

--- inpacks.py
+++ inpacks.py
@@ -10,30 +10,34 @@ PACKPATH = path.expanduser('~/.vim/pack/')
 
 URLS = (
 
+    # ('base/opt', 'https://github.com/kien/rainbow_parentheses.vim'),
     ('base/opt', 'https://github.com/Rip-Rip/clang_complete'),
     ('base/opt', 'https://github.com/dhruvasagar/vim-table-mode'),
     ('base/opt', 'https://github.com/fcpg/vim-waikiki'),
     ('base/opt', 'https://github.com/google/yapf'),
+    ('base/opt', 'https://github.com/jamessan/vim-gnupg'),
     ('base/opt', 'https://github.com/johngrib/vim-game-snake'),
     ('base/opt', 'https://github.com/junegunn/goyo.vim'),
     ('base/opt', 'https://github.com/m42e/vim-gcov-marker'),
+    ('base/opt', 'https://github.com/osyo-manga/vim-over'),
     ('base/opt', 'https://github.com/vim-pandoc/vim-pandoc'),
     ('base/opt', 'https://github.com/vim-pandoc/vim-pandoc-after'),
     ('base/opt', 'https://github.com/vim-pandoc/vim-pandoc-syntax'),
-    # ('base/opt', 'https://github.com/kien/rainbow_parentheses.vim'),
 
+    # actions/motions
     ('base/start', 'https://github.com/Raimondi/delimitMate'),
-    ('base/start', 'https://github.com/chrisbra/colorizer'),
     ('base/start', 'https://github.com/godlygeek/tabular'),
-    ('base/start', 'https://github.com/jamessan/vim-gnupg'),
-    ('base/start', 'https://github.com/lifepillar/vim-mucomplete'),
-    ('base/start', 'https://github.com/ludovicchabant/vim-gutentags'),
-    ('base/start', 'https://github.com/mhinz/vim-startify'),
-    ('base/start', 'https://github.com/osyo-manga/vim-over'),
     ('base/start', 'https://github.com/simnalamburt/vim-mundo'),
     ('base/start', 'https://github.com/tpope/vim-commentary'),
     ('base/start', 'https://github.com/tpope/vim-fugitive'),
     ('base/start', 'https://github.com/tpope/vim-surround'),
+
+    # noice
+    ('base/start', 'https://github.com/chrisbra/colorizer'),
+    ('base/start', 'https://github.com/lifepillar/vim-mucomplete'),
+    ('base/start', 'https://github.com/ludovicchabant/vim-gutentags'),
+    ('base/start', 'https://github.com/mhinz/vim-startify'),
+    ('base/start', 'https://github.com/unblevable/quick-scope'),
     ('base/start', 'https://github.com/w0rp/ale'),
 
     ('colorschemes/opt', 'https://github.com/morhetz/gruvbox'),
